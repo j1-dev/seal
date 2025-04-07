4ff483e80090f732655cabaace0beab77ca76b74
j1-dev <aephyxen@gmail.com>
2025-04-07 11:55:56 +0200

    Added morph component to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,14 @@
-'use client';
+import Morph from '@/components/morph';
 import Logo from '@/components/logo';
-import { Marquee } from '@/components/marquee';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
-export default function Home() {
-  const router = useRouter();
+export default function HomePage() {
   return (
-    <div className="h-screen w-full">
-      <div
-        className="p-0.5 rounded-2xl absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 bg-gradient-to-br 
-                 from-neutral-200 via-blue-200 to-pink-200 animated-background"
-        onClick={() => {
-          router.push('/login');
-        }}>
-        <div
-          className="h-96 w-96 rounded-2xl dark:bg-neutral-950 bg-neutral-100
-                     text-8xl font-black p-9 pt-[46px] transition-all 
-                     hover:bg-transparent hover:dark:bg-transparent hover:dark:text-neutral-900 duration-1000 
-                     cursor-wait group shadow hover:shadow-xl">
-          <Logo size={500} />
-        </div>
-      </div>
-      <Marquee>
-        <span className="text-2xl font-black italic">
-          ~ comming soon ~ comming soon ~ comming soon ~ comming soon ~ comming
-          soon ~ comming soon ~ comming soon ~ comming soon ~ comming soon ~
-        </span>
-      </Marquee>
+    <div className="flex items-center justify-center h-screen">
+      <Link href={'/login'}>
+        <Logo size={94} className="w-60 mx-auto" />
+        <Morph texts={['Hey!', 'Join us!']} />
+      </Link>
     </div>
   );
 }
---
+++ src/components/morph.tsx
@@ -0,0 +1,117 @@
+'use client'
+import React, { JSX, useEffect, useRef } from 'react';
+
+export default function Morph({ texts }: { texts: string[] }): JSX.Element {
+  const text1Ref = useRef<HTMLSpanElement>(null);
+  const text2Ref = useRef<HTMLSpanElement>(null);
+
+  const morphTime = 2;
+  const cooldownTime = 0.5;
+
+  let textIndex = texts.length - 1;
+  let lastFrameTime = performance.now();
+  let morph = 0;
+  let cooldown = cooldownTime;
+
+  useEffect(() => {
+    const elts = {
+      text1: text1Ref.current,
+      text2: text2Ref.current,
+    };
+
+    const doMorph = () => {
+      morph -= cooldown;
+      cooldown = 0;
+
+      let fraction = morph / morphTime;
+
+      if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+      }
+
+      setMorph(fraction);
+    };
+
+    const setMorph = (fraction: number) => {
+      if (elts.text2) {
+        elts.text2.style.filter = `blur(${Math.min(8 / fraction - 10, 100)}px)`;
+        elts.text2.style.opacity = `${Math.pow(fraction, 0.7) * 100}%`;
+      }
+
+      fraction = 1 - fraction;
+
+      if (elts.text1) {
+        elts.text1.style.filter = `blur(${Math.min(8 / fraction - 10, 100)}px)`;
+        elts.text1.style.opacity = `${Math.pow(fraction, 0.7) * 100}%`;
+        elts.text1.textContent = texts[textIndex % texts.length];
+      }
+
+      if (elts.text2) {
+        elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+      }
+    };
+
+    const doCooldown = () => {
+      morph = 0;
+
+      if (elts.text2) {
+        elts.text2.style.filter = '';
+        elts.text2.style.opacity = '100%';
+      }
+
+      if (elts.text1) {
+        elts.text1.style.filter = '';
+        elts.text1.style.opacity = '0%';
+      }
+    };
+
+    let animationFrame: number;
+
+    const animate = () => {
+      const shouldIncrementIndex = cooldown > 0;
+      const currentTime = performance.now();
+      const deltaTime = (currentTime - lastFrameTime) / 1000; // Convert to seconds
+      lastFrameTime = currentTime;
+
+      cooldown -= deltaTime;
+
+      if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+          textIndex++;
+        }
+        doMorph();
+      } else {
+        doCooldown();
+      }
+      animationFrame = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => cancelAnimationFrame(animationFrame);
+  }, []);
+
+  return (
+    <>
+      <div id="container">
+        <span id="text1" ref={text1Ref}></span>
+        <span id="text2" ref={text2Ref}></span>
+      </div>
+      <svg id="filters">
+        <defs>
+          <filter id="threshold">
+            <feColorMatrix
+              in="SourceGraphic"
+              type="matrix"
+              values="1 0 0 0 0
+                      0 1 0 0 0
+                      0 0 1 0 0
+                      0 0 0 255 -140"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </>
+  );
+}
